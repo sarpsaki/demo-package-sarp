66d1b5e909095016bc4fda868e8f819d7798758a
sarpsaki <sarpshucky@gmail.com>
2026-03-10 00:19:31 +0300

    fix: package update 3

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -209,4 +209,4 @@ class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
     name: Literal["DemoPackageSarp"] = "DemoPackageSarp"
-    uID: str = "1234567" 
+    uID: str = "1234567"
